23c80a6a299c473909e2faacd633394d45fbb401
Kojo Opuni <kojo.opuni@nyu.edu>
2013-09-21 12:05:39 -0400

    Final Mods to Menu GUI

--- C#/MainMenuGUI.cs
+++ C#/MainMenuGUI.cs
@@ -12,39 +12,73 @@ public class MainMenuGUI : MonoBehaviour {
 	public Rect quitButton;
 	
 	Rect menuAreaNormalized;
+	
+	public Rect instructions;
+	
+	string menuPage = "main";
 
 	// Use this for initialization
 	void Start () 
 	{
-	
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-	
+		menuAreaNormalized = new Rect (menuArea.x * Screen.width - (menuArea.width * 0.5f), menuArea.y * Screen.height - (menuArea.height * 0.5f), menuArea.width,menuArea.height);
 	}
 	
 	void OnGUI ()
 	{
 		GUI.skin = menuSkin;
-		GUI.BeginGroup(menuArea);
+		GUI.BeginGroup(menuAreaNormalized);
 		
-		if (GUI.Button (new Rect (playButton), "Play"))
+		if (menuPage == "main")
 		{
-			audio.PlayOneShot(beep);
+		
+			if (GUI.Button (new Rect (playButton), "Play"))
+			{
+				StartCoroutine("ButtonAction","Island");
+			}
+			
+			if (GUI.Button (new Rect (instructionsButton), "Instructions"))
+			{
+				audio.PlayOneShot(beep);
+				menuPage = "instructions";
+			}
+			
+			if (GUI.Button (new Rect (quitButton), "Quit"))
+			{
+				StartCoroutine("ButtonAction","quit");
+			}
+			
 		}
-		
-		if (GUI.Button (new Rect (instructionsButton), "Instructions"))
+		else if  (menuPage == "instructions")
 		{
-			audio.PlayOneShot(beep);
-		}
-		
-		if (GUI.Button (new Rect (quitButton), "Quit"))
-		{
-			audio.PlayOneShot(beep);
+			GUI.Label(new Rect(instructions), "You awake on a mysterious island..."+
+				"Find a way to signal for help or face certain doom!");
+			if (GUI.Button (new Rect (quitButton), "Back"))
+			{
+				audio.PlayOneShot(beep);
+				menuPage = "main";
+			}
 		}
 		
 		GUI.EndGroup();
+	}
+	
+	IEnumerator ButtonAction (string levelName)
+	{
+		audio.PlayOneShot(beep);
+		
+		yield return new WaitForSeconds(0.35f);
+		
+		if (levelName != "quit")
+		{
+			Application.LoadLevel(levelName);
+		}
+		else
+		{
+			Application.Quit();
+			Debug.Log("Have Quit");
+		}
+		
+		
+		
 	}
 }
